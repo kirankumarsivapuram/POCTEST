6cb99414864ddace217895099c777b6baf3b0282
kirankumarsivapuram <98744091+kirankumarsivapuram@users.noreply.github.com>
2023-08-28 15:52:40 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -22,68 +22,57 @@ client_secret  = var.client_secret
 
 }
 
-resource "azurerm_resource_group" "rg" {
-  name     = var.resource_group_name
-  location = var.location
+
+
+data "azurerm_resource_group" "resourcegroup" {
+    name = var.resource_group_name 
 }
 
-resource "azurerm_role_assignment" "role_acrpull" {
-  scope                            = azurerm_container_registry.acr.id
-  role_definition_name             = var.role_definition_name
-  principal_id                     = azurerm_kubernetes_cluster.aks.kubelet_identity.0.object_id
-  skip_service_principal_aad_check = var.Azure_Role_Assignment_skip_service_principal_aad_check
+data "azurerm_virtual_network" "vnet" {
+    name = var.Azure_Vnet_Name
+    resource_group_name = data.azurerm_resource_group.resourcegroup.name    
+  
 }
 
-resource "azurerm_virtual_network" "virtualnetwork" {
-  name = var.Azure_Vnet_Name
-  resource_group_name = azurerm_resource_group.rg.name
-  location = azurerm_resource_group.rg.location
-  address_space = [var.Azure_Vnet_address_space]
+data "azurerm_subnet" "subnet" {
+    name = var.Azure_AKS_Subnet_Name
+    resource_group_name = data.azurerm_resource_group.resourcegroup.name
+    virtual_network_name = data.azurerm_virtual_network.vnet.name
+  
 }
 
 resource "azurerm_subnet" "virtualsubnet" {
   name = var.Azure_Subnet_Name
-  virtual_network_name = azurerm_virtual_network.virtualnetwork.name
-  resource_group_name = azurerm_resource_group.rg.name
+  virtual_network_name = data.azurerm_virtual_network.vnet.name
+  resource_group_name = data.azurerm_resource_group.resourcegroup.name
   address_prefixes = [var.Azure_Subnet_Name_address_prefixes]
   
 }
+resource "azurerm_api_management" "apimaanagement" {
 
-resource "azurerm_container_registry" "acr" {
-  name                = var.acr_name
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = var.location
-  sku                 = var.azurerm_container_registry_sku
-  admin_enabled       = var.azurerm_container_registry_Admin_Enabled
+    name = var.api_management_name
+    location = data.azurerm_resource_group.resourcegroup.location
+    resource_group_name = data.azurerm_resource_group.resourcegroup.name
+    publisher_name = var.api_management_name
+    publisher_email = var.api_management_publisher_email
+    sku_name = var.sku_name
+    virtual_network_type = var.api_management_virtual_network_type
+    virtual_network_configuration {
+    subnet_id = azurerm_subnet.virtualsubnet.id
+   } 
 }
 
-resource "azurerm_kubernetes_cluster" "aks" {
-  name                = var.cluster_name
-  kubernetes_version  = var.kubernetes_version
-  location            = var.location
-  resource_group_name = azurerm_resource_group.rg.name
-  dns_prefix          = var.cluster_name
-  
-  default_node_pool {
-    name                = var.AKS_Default_Node_pool_Name
-    node_count          = var.system_node_count
-    vm_size             = var.AKS_Default_Node_pool_VM_Size
-    type                = "VirtualMachineScaleSets"
-    node_labels = {
-      
-      "nodepool-type" = "system"
-      "environment"   = "prod"
-      "nodepoolos"    = "linux"
-    }
-    os_disk_size_gb = "30"
-    #availability_zones  = [1, 2, 3]
-    enable_auto_scaling = var.AKS_Default_Node_pool_enable_auto_scale
-    vnet_subnet_id = azurerm_subnet.virtualsubnet.id
-  }
- 
-  identity {
-    type = "SystemAssigned"
-  }  
+resource "azurerm_api_management_gateway" "apigateway" {
 
+    api_management_id = azurerm_api_management.apimaanagement.id
+    name = var.api_management_gateway_name
+    location_data {
+      city = var.api_management_gateway_city
+      district = var.api_management_gateway_district
+      name = var.api_management_gateway_location_name
+      region = data.azurerm_resource_group.resourcegroup.location     
+    }    
 }
+
+
 
