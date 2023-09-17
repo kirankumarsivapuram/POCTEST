ff9790be207ef48b1c6090ea1a8607acd2024297
kirankumarsivapuram <98744091+kirankumarsivapuram@users.noreply.github.com>
2023-09-17 14:25:55 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -28,10 +28,4 @@ resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
   location =var.resource_group_location
 }
-resource "azurerm_container_registry" "acr" {
-  name                = var.acr_name
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = var.location
-  sku                 = var.azurerm_container_registry_sku
-  admin_enabled       = var.azurerm_container_registry_Admin_Enabled
-}
+
